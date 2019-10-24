459c26a9e0fd8787128bcafdcdb57013239e753f
Rajesh <rajbca00@gmail.com>
2019-10-24 18:41:14 +0530

    'auth_middleware_created'

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
 // @route   GET api/auth
 // @desc    Test route
 // @access  PUBLIC
-router.get('/', (req, res) => {
+router.get('/', auth, (req, res) => {
   res.send('from auth router');
 });
 

d02266e3b1339ef0daa94b5c83182baa118cbef5
Rajesh <rajbca00@gmail.com>
2019-10-24 15:27:52 +0530

    first commit

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+const mongoURI = config.get('mongoURI');
+
+const connectDB = async () => {
+  await mongoose
+    .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+      console.log('Connected to Database');
+    })
+    .catch(({ message }) => {
+      console.log(
+        'Failed to connect database. Process Exists. Error occured ' + message
+      );
+      process.exit(1);
+    });
+};
+
+module.exports = connectDB;
